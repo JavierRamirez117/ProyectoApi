5daa69fa11e934954bcf6c30d2ef7ca0eef161e1
Javier Ramirez <travi_javier@hotmail.com>
2021-01-12 15:00:20 -0600

    1.2 Implementando el login

---
+++ java/login.js
@@ -0,0 +1,24 @@
+const alreadyLoggedIn = localStorage.getItem('loggedIn')
+if(alreadyLoggedIn) {
+    window.location.href = "/API.html";
+}
+
+const users =[
+    {
+        email: 'testxD@hotmail.com',
+        password: 'test'
+    }
+];
+
+function login() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const user  = users.find((user) => user.email === email && user.password === password);
+    if(user) {
+        localStorage.setItem('loggedIn', 'true') 
+        window.location.replace("/API.html");
+    } else {
+        alert('Usuario y/o Contraseña incorrectos')
+    }
+}
